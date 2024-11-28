f690bacdfd137935c08587405deb3b1ea3afff79
Hannah Kellett <hannahkellett2@gmail.com>
2024-11-28 11:12:14 +0000

    Defined/made a few methods in treemap, basic testing stuff in main

--- ADS_2024_CA2_h_k/Treemap.h
+++ ADS_2024_CA2_h_k/Treemap.h
@@ -1,8 +1,43 @@
 #pragma once
+#include "Entity.h"
 template<class K, class V>
 class TreeMap {
 	BinaryTree<Entity<K, V>> map;
 public:
+	void clear();
+	bool containsKey(K key);
+	V& get(K key);
+	BinaryTree<K> keySet();
+	void put(K& key, V& value);
+	int size();
+	bool removeKey(K key);
+	V& operator[](K key);
+};
 
+template<class K, class V>
+void TreeMap<K, V>::put(K& key, V& value)
+{
+	Entity<K, V> e;
+	e.key = key;
+	e.value = value;
+	map.add(e);
+}
+template<class K, class V>
+V& TreeMap<K, V>::get(K key)
+{
+	Entity<K, V> e;
+	e.key = key;
+	return map.get(e).value;
+}
 
+template<class K, class V>
+void TreeMap<K, V>::clear()
+{
+	map.clear();
+}
+
+template<class K, class V>
+int TreeMap<K, V>::size()
+{
+	return map.count();
 }
--- ADS_2024_CA2_h_k/main.cpp
+++ ADS_2024_CA2_h_k/main.cpp
@@ -2,11 +2,20 @@
 #include <fstream>
 #include "BinaryTree.h"
 #include "BSTNode.h"
+#include "TreeMap.h"
 
 using namespace std;
 
 int main()
 {
-	BinaryTree<string> tree;
+	TreeMap<int, string> tree;
+	int x = 1, y = 2;
+	string s1 = "One", s2 = "two";
+
+	tree.put(x, s1);
+	tree.put(y, s2);
+
+	cout << tree.get(y) << endl;
+
 	return 0;
 }
