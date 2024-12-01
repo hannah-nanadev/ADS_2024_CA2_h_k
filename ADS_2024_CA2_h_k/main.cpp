e69db73d3af34dd3283754082a88507322abaea7
hannah-nanadev <hannahkellett2@gmail.com>
2024-12-01 22:04:03 +0000

    🚧 implemented reading txt file for words and puts them in a string, vector<string> wordmap - display to be completed

--- ADS_2024_CA2_h_k/main.cpp
+++ ADS_2024_CA2_h_k/main.cpp
@@ -14,6 +14,26 @@ int main()
 	if(fin)
 	{
 		cout << "File loaded successfully!" << endl;
+		const char delim = ' '; string word; string firstChar;
+		while (fin >> word)
+		{
+			firstChar = word.at(0);
+
+			try {
+				if (wordMap.containsKey(firstChar))
+				{
+					wordMap.get(firstChar).push_back(word);
+				}
+			}
+			catch (logic_error)
+			{
+				vector<string> newVector = { word };
+				wordMap.put(firstChar, newVector);
+			}
+
+		}
+
+		cout << "List of letters:" << endl;
 
 	}
 	else
