6c535ac1c5b2d880cfc824df8871885fac7edc2f
Hannah Kellett <hannahkellett2@gmail.com>
2024-12-04 16:13:06 +0000

    🚧 Made struct, initialised seperate treemaps, no file reading yet/parsing into the maps

--- ADS_2024_CA2_h_k/main.cpp
+++ ADS_2024_CA2_h_k/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "Treemap.h"
+#include "BinaryTree.h"
 
 using namespace std;
 int selectField();
@@ -14,10 +15,32 @@ void display(TreeMap<K, V>& map)
 	// display VAlues
 }
 
+struct App
+{
+	int id;
+	string appName;
+	string developer;
+	string platform;
+	string releaseDate;
+	float price;
+};
 
 int main()
 {
 	int selection = selectField();
+
+	if (selection == 1)
+	{
+		TreeMap<int, BinaryTree<App*>> map;
+	}
+	else if (selection == 6)
+	{
+		TreeMap<float, BinaryTree<App*>> map;
+	}
+	else
+	{
+		TreeMap<string, BinaryTree<App*>> map;
+	}
 
 	return 0;
 }
@@ -29,14 +52,15 @@ int selectField()
 	while (run)
 	{
 		cout << "Select one of the following:" << endl;
-		cout << "1 - " << endl;
-		cout << "2 - " << endl;
-		cout << "3 - " << endl;
-		cout << "4 - " << endl;
-		cout << "5 - " << endl;
+		cout << "1 - ID" << endl;
+		cout << "2 - App Name" << endl;
+		cout << "3 - Developer" << endl;
+		cout << "4 - Platform" << endl;
+		cout << "5 - Release Date" << endl;
+		cout << "6 - Price" << endl;
 
 		cin >> selection;
-		if (selection <= 0 || selection >= 6)
+		if (selection <= 0 || selection >= 7)
 		{
 			cout << "Invalid input. Please try again." << endl;
 			cin.clear();
